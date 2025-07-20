14d6a88c26c62066ff0b30e64c4cea2ebb47b4d7
NatanVinicius <natanviniciusdepaula@gmail.com>
2025-07-20 20:35:22 +1200

    ADD TERRAFORM and WORKFLOW

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "gcs" {
+    bucket  = "rds-campervan-terraform-state"
+    prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version: "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+    project = var.project_id
+    region = var.region
+}
+
+resource "google_cloud_run_service" "backend" {
+    name = "campervan-backend"
+    location = var.region
+
+    template {
+        spec {
+            containers {
+                image = var.backend_image
+                ports {
+                    container_port = 3000
+                }
+            }
+        }
+    }
+
+    traffic {
+        percent = 100
+        latest_revision = true
+    }
+}
+
+resource "google_cloud_run_service_iam_member" "invoiker" {
+    service = google_cloud_run_service.backend.name
+    location = var.region
+    role = "roles/run.invoker"
+    member = "allUsers"
+}
+
+resource "google_artifact_registry_repository" "backend_repo" {
+  provider = google
+  location = var.region
+  repository_id = "campervan-backend"
+  format = "DOCKER"
+  description = "Docker image repo for Campervan backend"
+}
+
+resource "google_cloud_run_service" "backend_service" {
+  name     = "campervan-backend"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/campervan-backend/backend:latest"
+
+        ports {
+          container_port = 3000
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  autogenerate_revision_name = true
+}
+
+resource "google_cloud_run_service_iam_member" "all_users_invoker" {
+  service  = google_cloud_run_service.backend_service.name
+  location = google_cloud_run_service.backend_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
