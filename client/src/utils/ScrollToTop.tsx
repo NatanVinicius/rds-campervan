9917cc6fc6d8b2e5df33e5ae960cadbb2fdd03c4
NatanVinicius <natanviniciusdepaula@gmail.com>
2025-07-20 18:53:05 +1200

    Updated front-end and workflow github

--- client/src/utils/ScrollToTop.tsx
+++ client/src/utils/ScrollToTop.tsx
@@ -4,7 +4,6 @@ import { useLocation } from "react-router-dom";
 export const ScrollToTop = () => {
 	const { pathname } = useLocation();
 
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 	}, [pathname]);
