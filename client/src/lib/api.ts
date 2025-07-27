c9f3839ad1f59f52cf4a15cec11175208bc2821e
NatanVinicius <natanviniciusdepaula@gmail.com>
2025-07-27 14:35:38 +1200

    fix: inject VITE_API_URL in Docker build

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-// export const api = axios.create({
-// 	baseURL: import.meta.env.VITE_API_URL,
-// });
-
 console.log(
 	"DEBUG: import.meta.env.VITE_API_URL",
 	import.meta.env.VITE_API_URL,
