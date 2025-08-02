b02eab8f7a68e507b29d0ca91f2bd92031079007
NatanVinicius <natanviniciusdepaula@gmail.com>
2025-08-02 21:22:36 +1200

    Page about us created and working

--- client/src/pages/aboutus/TextType.tsx
+++ client/src/pages/aboutus/TextType.tsx
@@ -106,7 +106,7 @@ const TextType = ({
 	useEffect(() => {
 		if (!isVisible) return;
 
-		let timeout: NodeJS.Timeout;
+		let timeout: number;
 
 		const currentText = textArray[currentTextIndex];
 		const processedText = reverseMode
